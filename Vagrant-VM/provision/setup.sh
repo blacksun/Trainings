e8b7cf1321935dbba1b4e009eedda3d7d7206dd2
Walid MNASRI <mnasri.walid@gmail.com>
2015-10-13 23:02:08 +0400

    update config vagrant

--- Vagrant-VM/provision/setup.sh
+++ Vagrant-VM/provision/setup.sh
@@ -10,9 +10,3 @@ wget http://downloads.zend.com/zendserver/8.5.1/ZendServer-8.5.1-RepositoryInsta
 tar zxf ./ZendServer-8.5.1-RepositoryInstaller-linux.tar.gz
 cd ./ZendServer-RepositoryInstaller-linux/
 yes | sudo ./install_zs.sh 5.6
-echo "ServerName localhost" > /etc/apache2/conf-enabled/servername.conf
-a2enmod suexec
-a2dissite 000-default.conf
-service apache2 restart
-
-
